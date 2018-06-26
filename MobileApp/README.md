b590c5b76afb27baa7cb9ace5823e2fded0a86a2
Suvrat Jain <jain.suvrat97@gmail.com>
2018-06-26 13:50:22 -0400

    New Commit

---
+++ MobileApp/README.md
@@ -0,0 +1,3 @@
+# RockPaperScissorApp
+This is a class project. The app is imitating the game of Rock-Paper-Scissor. Essentially, two users enter a lobby where the system randomly assigns them a game session. 
+The users then compete against each other by entering their option (i.e. Rock, Paper, or Scissor) on the app and sending it to the webpage. The webpage recieves the commands, displays which user won, and allocates the points accordingly. The winner is selected based on best 2 out of 3 basis. 
---
+++ MobileApp/app/src/main/java/com/example/suvratjain/firstapp/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.suvratjain.firstapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String dummyUsername = "test";
+    public static final String dummypassword = "test";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user taps the Login button */
+    public void login(View view) {
+
+        boolean authorization = false;
+
+        //get the fields by their ID mentioned in XML
+        EditText usernameValue = findViewById(R.id.usernameField);
+        EditText passwordValue = findViewById(R.id.passwordField);
+
+        //retrieve the strings typed in the fields
+        String usernameFieldValue = usernameValue.getText().toString();
+        String passwordFieldValue = passwordValue.getText().toString();
+
+        //check the username and password and set the authorization to true
+        if(usernameFieldValue.equals(dummyUsername))
+            if(passwordFieldValue.equals(dummypassword))
+                authorization = true;
+
+
+        if(authorization)
+        {
+            Intent intent = new Intent(this, GameScreen.class);
+            startActivity(intent);
+        }
+
+    }
+}

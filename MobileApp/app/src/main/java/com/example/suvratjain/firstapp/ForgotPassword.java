4c7fbb7c8a288d875378a1a9a6972f1d73d4efb8
Suvrat Jain <jain.suvrat97@gmail.com>
2018-07-19 00:53:16 -0400

    forgot password updated

--- MobileApp/app/src/main/java/com/example/suvratjain/firstapp/ForgotPassword.java
+++ MobileApp/app/src/main/java/com/example/suvratjain/firstapp/ForgotPassword.java
@@ -2,6 +2,7 @@ package com.example.suvratjain.firstapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ForgotPassword extends AppCompatActivity {
 
@@ -9,5 +10,14 @@ public class ForgotPassword extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
+    }
+
+    //return to main activity
+    public void back(View view) {
+        finish();
+    }
+
+    //get password
+    public void getPassword(View view) {
     }
 }
